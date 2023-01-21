69d1afe21e6bfc97355e4de939a9127c7641f42c
shivprajapat <shivkummarprajapat232@gmail.com>
2023-01-21 18:06:06 +0530

    global theme color variable , light & dark mode functionality add

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { ColorModeContext, useMode } from "./theme";
 import { Dashboard, Sidebar, Topbar } from "./scenes";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 const App = () => {
+  const [theme, colorMode] = useMode();
+
   return (
-    <div className="app">
-      <Sidebar />
-      <main className="content">
-        <Topbar />
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-        </Routes>
-      </main>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar />
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   )
 }
 
