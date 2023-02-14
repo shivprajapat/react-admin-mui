6d2867d172e25d99c2e3c9e078e140de1aa52f02
shivprajapat <shivkummarprajapat232@gmail.com>
2023-02-14 18:30:45 +0530

    geography & line chart

--- src/scenes/geography/index.js
+++ src/scenes/geography/index.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import { Box } from '@mui/system'
-import { Header } from "../../components";
+import { GeographyChart, Header } from "../../components";
+import { tokens } from '../../theme';
+import { useTheme } from '@emotion/react';
 
 const Geography = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
   return (
     <Box>
       <Header title="Geography" subtitle="Simple Geography Chart" />
+      <Box
+        height="75vh"
+        border={`1px solid ${colors.grey[100]}`}
+        borderRadius="4px"
+      >
+        <GeographyChart />
+      </Box>
     </Box>
   )
 }
--- src/scenes/line/index.js
+++ src/scenes/line/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Box } from '@mui/system'
-import { Header } from "../../components";
+import { Header, LineChart } from "../../components";
 
 const Line = () => {
   return (
     <Box>
       <Header title="Line Chart" subtitle="Simple Line Chart" />
+      <Box height="75vh">
+        <LineChart />
+      </Box>
     </Box>
   )
 }
